b965a8f9b814ed0f99ab710a19a51db2fa3840a1
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-01-24 13:58:16 +0300

    Add new files and make styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import windows11Wallpaper from '$lib/assets/windows11.png';
+</script>
+
+<div class="relative w-full h-screen">
+	<img
+		class="w-full h-screen absolute top-0 left-0 -z-10"
+		src={windows11Wallpaper}
+		alt="windows wallpaper"
+	/>
+</div>
